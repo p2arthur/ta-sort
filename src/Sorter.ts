514c60192008b47d65f35a59da727b5fa961bb54
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-11-22 12:02:57 -0300

    (feat) add timing function to get how much time our sorting algorithm is taking on each collection

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,5 +1,3 @@
-import { NumberCollection } from './NumbersCollection';
-
 export interface Sortable {
   length: number;
   compare(leftIndex: number, rightIndex: number): boolean;
@@ -7,19 +5,21 @@ export interface Sortable {
 }
 
 export class Sorter {
-  constructor(public collection: Sortable) {}
-
   //Bubble sort
-  public sortCollection(): void {
-    const { length } = this.collection;
+  public sortCollection(collection: Sortable): void {
+    const t1 = performance.now();
+    const { length } = collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        const shouldSwap = this.collection.compare(j, j + 1);
+        const shouldSwap = collection.compare(j, j + 1);
         if (shouldSwap) {
-          this.collection.swap(j, j + 1);
+          collection.swap(j, j + 1);
         }
       }
     }
+
+    const t2 = performance.now();
+    console.log(`The function took ${(t2 - t1) / 1000} seconds to run`);
   }
 }
